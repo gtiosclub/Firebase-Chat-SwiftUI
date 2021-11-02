265b2f5bd4de81097e10047dc72c7e1b7690b3ee
Jai Chawla <jaichawla2012@gmail.com>
2021-11-02 17:15:28 -0400

    Create firebase chat app in SwiftUI

--- Firebase Chat/ContentView.swift
+++ Firebase Chat/ContentView.swift
@@ -8,9 +8,50 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var text = ""
+    @State var messages: [Message] = []
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                HStack {
+                    TextField("Message", text: self.$text)
+                    
+                    Button(action: {
+                        self.send()
+                    }, label: {
+                        Text("Send")
+                    })
+                }
+                .padding()
+                
+                List {
+                    ForEach(messages) { message in
+                        MessageListView(message: message)
+                    }
+                }
+            }
+            .navigationTitle("Firebase Chat")
+            .onAppear {
+                fetchMessages()
+            }
+        }
+        .navigationViewStyle(StackNavigationViewStyle())
+        
+    }
+    
+    func send() {
+        let time = Date().timeIntervalSince1970
+        Message(text: self.text, time: time).send()
+        self.text = ""
+    }
+    
+    func fetchMessages() {
+        Message.observe { message in
+            self.messages.insert(message, at: 0)
+            self.messages.sort(by: { (m1, m2) -> Bool in
+                return m1.time < m2.time
+            })
+        }
     }
 }
 
