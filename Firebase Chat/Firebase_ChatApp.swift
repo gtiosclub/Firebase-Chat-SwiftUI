18c62ee501b6338b657058c721f38db4fbd9f2f4
Jai Chawla <jaichawla2012@gmail.com>
2021-11-02 16:35:20 -0400

    Initial Commit

---
+++ Firebase Chat/Firebase_ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_ChatApp.swift
+//  Firebase Chat
+//
+//  Created by Jai Chawla on 11/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
